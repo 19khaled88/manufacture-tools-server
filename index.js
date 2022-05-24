845428910902415c9a740e8b233a32c8b37fa08f
19khaled88 <69922782+19khaled88@users.noreply.github.com>
2022-05-25 02:24:19 +0600

    proudct sold and stock updated

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer')
 const app = express()
 const port = process.env.PORT || 5000
+ObjectID = require('mongodb').ObjectId
 
 //middleware
 app.use(cors())
@@ -11,8 +12,9 @@ app.use(express.json())
 let user = process.env.DB_USER
 let pass = process.env.DB_PASS
 
-const { MongoClient, ServerApiVersion } = require('mongodb')
-const uri = `mongodb+srv://khaled:VNHAybzMnVDF6NMq@cluster0.ka5da.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+// const { ObjectID } = require('bson')
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ka5da.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -26,8 +28,9 @@ async function run() {
     const productCollection = client
       .db('toolsmanufacture')
       .collection('products')
+    const ratingCollection = client.db('toolsmanufacture').collection('rating')
+    const soldCollection = client.db('toolsmanufacture').collection('sold')
 
-    
     //index products
     app.get('/product', async (req, res) => {
       const query = {}
@@ -46,38 +49,68 @@ async function run() {
     })
 
     //place order
-    app.post('/placceOrder',cors(),async(req, res)=>{
-      const order = req.body.ordered;
-      const price = req.body.price;
-      const email = req.body.mail;
-      const phone = req.body.phone;
-      const address = req.body.address;
-      try {  
-        var transport = nodemailer.createTransport({
-          host:process.env.SMTP_SERVER,
-          port:process.env.SEND_IN_BLUE_PORT,
-          auth:{
-            user:process.env.SEND_IN_BLUE_LOGIN, 
-            pass:process.env.SEND_IN_BLUE_MASTER_PASS 
+    app.post('/placceOrder', cors(), async (req, res) => {
+      const name = req.body.name
+      const email = req.body.mail
+      const address = req.body.address
+      const order = req.body.ordered
+      const price = req.body.price
+      const phone = req.body.phoneNumber
+      const stock = req.body.stockValue
+      const product = req.body.product
+      const id = req.body.id
 
-          }
-        })
+      const insert = { name, product, email, address, phone, order, price }
+      const result = await soldCollection.insertOne(insert)
+      if (result) {
+        const filter = { _id: ObjectId(id) }
+        const options = { upsert: true }
+        const updateDoc = {
+          $set: {
+            product_stock: stock,
+          },
+        }
 
-        let mailStatus = await transport.sendMail({
-          from :'kkhaled88hasan@gmail.com',
-          to: email,
-          subject: 'Send from abc manufacturer company',
-          text: 'Body message area'
-        });
-
-        return `Message sent: ${mailStatus.messageId}`;
-        
-      } catch (error) {
-        
+        const result = await productCollection.updateOne(
+          filter,
+          updateDoc,
+          options,
+        )
+        res.send('Order submited successfully')
       }
-    
-    })
+      // try {
+      //   var transport = nodemailer.createTransport({
+      //     host: process.env.SMTP_SERVER,
+      //     port: process.env.SEND_IN_BLUE_PORT,
+      //     auth: {
+      //       user: process.env.SEND_IN_BLUE_LOGIN,
+      //       pass: process.env.SEND_IN_BLUE_MASTER_PASS,
+      //     },
+      //   })
 
+      //   let mailStatus = await transport.sendMail({
+      //     from: 'kkhaled88hasan@gmail.com',
+      //     to: email,
+      //     subject: 'Send from abc manufacturer company',
+      //     text: 'Body message area',
+      //   })
+
+      //   return `Message sent: ${mailStatus.messageId}`
+      // } catch (error) {}
+    })
+    //review and rating create
+    app.post('/rating', async (req, res) => {
+      const rate = req.body
+      const result = await ratingCollection.insertOne(rate)
+      res.send(result)
+    })
+    //review and rating index
+    app.get('/rating', async (req, res) => {
+      const query = {}
+      const cursor = ratingCollection.find(query)
+      const rating = await cursor.toArray(cursor)
+      res.send(rating)
+    })
   } finally {
   }
 }

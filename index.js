d214a92da6edf2340712b3e759d91726e06e2369
19khaled88 <69922782+19khaled88@users.noreply.github.com>
2022-05-27 15:11:24 +0600

    mangage orders and manage product

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer')
 const app = express()
 const port = process.env.PORT || 5000
 const jwt = require('jsonwebtoken')
-ObjectID = require('mongodb').ObjectId
+// const ObjectId = require('mongodb').ObjectId
 
 //middleware
 app.use(cors())
@@ -48,6 +48,9 @@ async function run() {
     const ratingCollection = client.db('toolsmanufacture').collection('rating')
     const soldCollection = client.db('toolsmanufacture').collection('sold')
     const userCollection = client.db('toolsmanufacture').collection('users')
+    const profileCollection = client
+      .db('toolsmanufacture')
+      .collection('profile')
 
     //index products
     app.get('/product', async (req, res) => {
@@ -61,7 +64,7 @@ async function run() {
     app.put('/user/:email', async (req, res) => {
       const user = req.body
       const email = req.params.email
-      console.log(email)
+
       const filter = { email: email }
       const options = { upsert: true }
       const updateDoc = {
@@ -123,8 +126,9 @@ async function run() {
       const phone = req.body.phoneNumber
       const stock = req.body.stockValue
       const product = req.body.product
+      const pay = req.body.payment
       const id = req.body.id
-      const insert = { name, product, email, address, phone, order, price }
+      const insert = { name, product, email, address, phone, order, price, pay }
       const result = await soldCollection.insertOne(insert)
       if (result) {
         const filter = { _id: ObjectId(id) }
@@ -210,6 +214,64 @@ async function run() {
         return res.send(result)
       } else {
         return res.status(403).send({ message: 'forbidden access' })
+      }
+    })
+
+    app.put('/updateProfile/:email', async (req, res) => {
+      const data = req.body
+      const email = req.params.email
+      const filter = { email: email }
+      const options = { upsert: true }
+      const updateDoc = { $set: data }
+      const result = await profileCollection.updateOne(
+        filter,
+        updateDoc,
+        options,
+      )
+      const token = jwt.sign({ email: email }, process.env.WEB_TOKEN, {
+        expiresIn: '1h',
+      })
+      res.send({ result, token })
+    })
+    app.get('/updateProfile/:email', async (req, res) => {
+      const email = req.params.email
+
+      const result = await profileCollection.findOne({ email: email })
+      if (result) {
+        res.send(result)
+      } else {
+        res.send({ message: 'data empty' })
+      }
+    })
+    app.put('/manageProduct/:id', async (req, res) => {
+      const id = req.params.id
+      const data = req.body
+      const filter = { _id: ObjectId(id) }
+      const options = { upsert: true }
+      const updateDoc = { $set: data }
+      const result = await productCollection.updateOne(
+        filter,
+        updateDoc,
+        options,
+      )
+      res.send(result)
+    })
+    // manage orders
+    app.get('/manageorders', async (req, res) => {
+      const query = {}
+      const cursor = soldCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+    app.delete('/deleteSoldProduct/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+
+      const result = await soldCollection.deleteOne(query)
+      if (result.deletedCount === 1) {
+        res.status(200).send(result)
+      } else {
+        res.status(400).send({ message: 'delete unsuccessful' })
       }
     })
   } finally {
